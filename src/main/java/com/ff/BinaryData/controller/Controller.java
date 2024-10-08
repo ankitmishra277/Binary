abee83b67be2c0d338d9e3a69ca5fe33fe755c2d
ankitmishra277 <ankit7000mishra@gmail.com>
2024-10-08 11:08:50 +0530

    Update Controller.java

--- src/main/java/com/ff/BinaryData/controller/Controller.java
+++ src/main/java/com/ff/BinaryData/controller/Controller.java
@@ -2,6 +2,7 @@ package com.ff.BinaryData.controller;
 
 import com.ff.BinaryData.service.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
